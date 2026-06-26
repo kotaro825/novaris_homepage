e16b3f26a87a44ec45cf4cf28dccfdc4e67711d9
m255346okuda <m255346@hiroshima-u.ac.jp>
2026-06-26 17:23:28 +0900

    Initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,39 @@
+AUTHOR = 'Novaris'
+SITENAME = '星のソムリエ®︎・Novarisの観測所'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Asia/Tokyo'
+
+DEFAULT_LANG = 'ja'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = [
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+]
+
+# Social widget
+#SOCIAL = (
+#  ('twitter', 'https://x.com/novaris_sn'),
+ #   ('instagram', 'https://www.instagram.com/novaris_sn/'),
+#)
+
+AUTHOR_TWITTER = 'novaris_sn'
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+THEME = 'themes/pelican-mediumfox'
+DISPLAY_PAGES_ON_MENU = True
